45774d7c40e0f45c9d6c7af7d7e5547c2e826eda
DylanBloemendaal <dylan1506@email.com>
2024-01-16 11:22:09 +0100

    Uploading files

---
+++ HammingCodeProject.py
@@ -0,0 +1,122 @@
+# Dylan Bloemendaal     9485961
+# Hugo van Hattem       1957074
+
+# ###############################FUNCTIONS#####################################
+
+# TODO: Create classes to perform calculations with matrices and vectors whose
+#       elements are 0 or 1. Your classes should support at least addition and
+#       matrix multiplication.
+
+class Vector:
+    """Working: V + V"""
+    def __init__(self, lst):
+        self.lst = lst
+
+    def __str__(self):
+        return str(self.lst)
+
+    def __add__(self, other):
+
+        if len(self.lst) != len(other.lst):
+            raise TypeError("Cannot add vectors of different sizes")
+
+        vector_sum = []
+        for bit1, bit2 in zip(self.lst, other.lst):
+            if bit1 + bit2 == 1:
+                vector_sum.append(1)
+            else:
+                vector_sum.append(0)
+
+        return Vector(vector_sum)
+
+    def __mul__(self, other):
+        return
+
+
+class Matrix:
+    """Working: M + M, M * M, M * V"""
+    def __init__(self, lst):
+        self.lst = lst
+
+    def __str__(self):
+        return str(self.lst)
+
+    def __add__(self, other):
+
+        if len(self.lst) != len(other.lst) or len(self.lst[0]) != len(other.lst[0]):
+            raise TypeError("Matrix dimensions do not support addition")
+
+        matrix_sum = []
+
+        for row1, row2 in zip(self.lst, other.lst):
+            matrix_sum_row = []
+
+            for element1, element2 in zip(row1, row2):
+                matrix_sum_row.append(element1 + element2)
+
+            matrix_sum.append(matrix_sum_row)
+
+        return Matrix(matrix_sum)
+
+    def __mul__(self, other):
+
+        if len(self.lst[0]) != len(other.lst):
+            raise TypeError("Matrix/Vector dimensions do not support multiplication")
+
+        # Multiplying two matrices
+        if isinstance(other, Matrix):
+            matrix_product = []
+
+            for row_indx in range(len(self.lst)):
+                matrix_product_row = []
+
+                for column_indx in range(len(other.lst[0])):
+                    element = 0
+
+                    for indx in range(len(other.lst)):
+                        element += self.lst[row_indx][indx] * other.lst[indx][column_indx]
+
+                    matrix_product_row.append(element % 2)
+
+                matrix_product.append(matrix_product_row)
+
+            return Matrix(matrix_product)
+
+        # Multiplying a matrix with a vector
+        else:
+            vector_output = []
+
+            for row_indx in range(len(self.lst)):
+                element = 0
+
+                for indx in range(len(other.lst)):
+                    element += self.lst[row_indx][indx] * other.lst[indx]
+
+                vector_output.append(element % 2)
+
+        return Vector(vector_output)
+
+
+# TODO: Write functions that can encode, decode, and possibly correct messages
+#       using the parity bit and the Hamming(7,4) code.
+
+# TODO: Write a function that randomly converts a given number of bits to test
+#       your code.
+
+# #################################MAIN######################################## 123
+
+# TODO: Your code should be able to translate a given message, provided as a string,
+#       into binary format and be able to encode it, correct it if necessary and decode
+#       it again.
+
+# TODO: Extend your code to handle more complex Hamming codes.
+
+# TODO: Implement the functions also based on bitwise operations and compare
+# the speed with the matrix implementation.
+
+
+x = Matrix([[1,0,0,0], [0,1,0,0]])*Vector([1,0,1,1])
+y = Vector([1,0,1,0,0,1])+Vector([0,0,1,1,1,1])+Vector([1,0,1,0,0,1])
+
+print(x, type(x))
+print(y, type(y))
